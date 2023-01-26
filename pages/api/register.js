1433d447838474a365b1de94a9a614e1bf715866
Ghazian Fadhli Fakhrusy <fadhlighazian@gmail.com>
2023-01-26 12:09:31 +0700

    add CORS settings 1.7

--- pages/api/register.js
+++ pages/api/register.js
@@ -10,11 +10,6 @@ export default async (req, res) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Credentials': true,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        'Access-Control-Allow-Headers':
-          'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
       },
       body: JSON.stringify({ username, email, password }),
     });
@@ -32,19 +27,6 @@ export default async (req, res) => {
           sameSite: 'strict',
           path: '/',
         })
-      );
-
-      res.setHeader('Access-Control-Allow-Credentials', true);
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      // another common pattern
-      // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-      );
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
       );
 
       // Send back to user
