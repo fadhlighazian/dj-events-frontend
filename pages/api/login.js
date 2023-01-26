a21c3c7c0cbc1ab8eca6c91bbbec82a7330daa53
Ghazian Fadhli Fakhrusy <fadhlighazian@gmail.com>
2023-01-26 12:48:40 +0700

    add CORS settings 2.1

--- pages/api/login.js
+++ pages/api/login.js
@@ -27,6 +27,20 @@ export default async (req, res) => {
         })
       );
 
+      // cors
+      res.setHeader('Access-Control-Allow-Credentials', true)
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      // another common pattern
+      // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+      res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+      );
+      res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      );
+
       // Send back to user
       res.status(200).json({ user: data.user });
     } else {
